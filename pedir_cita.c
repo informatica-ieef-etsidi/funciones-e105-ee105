3f524561dc6131e8dd1fea53ad8a49eebbc45116
paula-arellano <36606152+paula-arellano@users.noreply.github.com>
2018-04-17 22:39:05 +0200

    Add files via upload

---
+++ pedir_cita.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+void pedir_cita();
+
+struct cita {
+	char usuario[15];
+	char fecha[10];
+};
+
+void main() {
+	pedir_cita();
+}
+
+void pedir_cita() {
+	struct cita cita;
+	int i = 0, j;
+
+	printf(" Nombre del paciente: ");
+	gets(cita.usuario);
+
+	printf(" \nFecha de la cita (formato Mes-Dia-Hora): ");
+	gets(cita.fecha);
+
+	FILE * citas;
+
+	errno_t error;
+	
+	//Fichero
+
+	error = fopen_s(&citas, "citas.txt", "w");
+
+	if (error == 0) {
+
+		printf("Cita concertada correctamente\n");
+
+	}
+
+	else {
+
+		printf("Ha habido un error %d al abrir el fichero\n", error);
+
+		system("pause");
+
+		return; //sale del programa
+
+	}
+
+	fprintf(citas, "%c\n", cita.usuario);
+	fprintf(citas, "%c\n\n", cita.fecha);
+
+	fclose(citas);
+
+}
