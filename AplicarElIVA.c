4d130473008c64b8a242dd55465353de064e6527
AnaDue99 <36692754+AnaDue99@users.noreply.github.com>
2018-04-17 17:24:15 +0200

    Aplicar el IVA

---
+++ AplicarElIVA.c
@@ -0,0 +1,33 @@
+//Desarrollador: Ana Dueñas
+//Programa para aplicar el iva a un producto
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float iva(float valor); // Prototipo o definicion de la funcion
+
+void main() {
+	float precio, preciofinal;
+	printf("Introduzca el precio base de su producto\n");
+	scanf_s("%f", &precio);
+
+	while (precio <= 0) {
+		printf("El precio debe ser positivo, vuelva a introducir el precio\n");
+		scanf_s("%f", &precio);
+		getchar();
+	}
+
+	preciofinal = iva(precio);
+	printf("El precio con iva seria de %.3f\n", preciofinal);
+	system("PAUSE");
+}
+
+	float iva(float valor) {
+		float resultado;
+	
+	resultado = valor * 1.21; //Suponiendo que el iva sea del 21%
+
+	
+	return resultado;
+}
