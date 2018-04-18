183e8fa9a96a95ac8a55e8329ad1e98678ad03dd
Marcos Velázquez Ruiz <36637024+MrCaos@users.noreply.github.com>
2018-04-18 11:13:57 +0200

    Ejercicio 11

---
+++ Ejercicio11.c
@@ -0,0 +1,25 @@
+// Marcos Velázquez Ruiz
+
+#include <stdio.h>
+
+int Sumatorio(int n, int a, int b);
+void main() {
+
+	int n, a, b, resultado;
+	printf("Introduce un n%cmero\n", 163);
+	scanf_s("%d", &n);
+	do {
+		printf("Introduce los extremos inferior y superior del sumatorio \n");
+		scanf_s("%d %d", &a, &b);
+	} while (a > b);
+	resultado = Sumatorio(n, a, b);
+	printf("Resultado = %d\n", resultado);
+
+	system("pause");
+}
+int Sumatorio(int n, int a, int b) {		int resultado = 0, suma = 0, i;
+	for (i = a; i<= b; i++) {
+		suma = suma + n*i;
+		resultado = suma;
+
+	}	return resultado;}
