91ca386ff5b272ff59811670c448ff6f90679351
paula-arellano <36606152+paula-arellano@users.noreply.github.com>
2018-04-19 00:22:51 +0200

    Add files via upload

---
+++ Ej_2.c
@@ -0,0 +1,27 @@
+//Paula Arellano
+#include<stdio.h>
+
+int sumatorioN(int n);
+
+void main() {
+	int numero, suma;
+
+	printf("Introduzca un numero entero: ");
+	scanf_s("%d", &numero);
+
+	suma = sumatorioN(numero);
+
+	printf("\nEl sumatorio de los %d primeros numeros es %d\n", numero, suma);
+
+	system("PAUSE");
+}
+
+int sumatorioN(int n) {
+	int i, resultado = 0;
+
+	for (i = 0; i <= n; i++) {
+		resultado = resultado + i;
+	}
+
+	return resultado;
+}
