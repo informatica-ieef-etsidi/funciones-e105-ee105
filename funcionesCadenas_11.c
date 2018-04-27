81b70ff8f8bbfd6211347fee3810c225939babb0
mariabosaba <36692966+mariabosaba@users.noreply.github.com>
2018-04-27 18:43:54 +0200

    Update funcionesCadenas_11.c

--- funcionesCadenas_11.c
+++ funcionesCadenas_11.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-/*11. Miro Cadena. Realizar una función que cuente la cantidad de caracteres de una cadena
-introducida por el usuario y que además sustituya los espacios en blanco por ‘_’ .
+/*11. Miro Cadena. Realizar una funciÃ³n que cuente la cantidad de caracteres de una cadena
+introducida por el usuario y que ademÃ¡s sustituya los espacios en blanco por â€˜_â€™ .
 void mirocadena (char cad[], int *veces);*/
 void mirocadena(char cad[], int *veces);
 void main() {
@@ -19,10 +19,10 @@ void mirocadena(char cad[], int *veces) {
 	*veces = 0;
 	i = 0;
 	while (cad[i] != '\0') {
-		i++;
 		*veces = *veces + 1;
 		if (cad[i] == ' ') {
 			cad[i] = '_';
 		}
+		i++;
 	}
 }
